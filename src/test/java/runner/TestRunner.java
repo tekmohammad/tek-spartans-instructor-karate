a776e8623cdf1aca1dd23579f03063970f5ad186
Mohammad Shokriyan <mohammad@tekschool.us>
2024-09-24 21:00:28 -0400

    prepare for jenkins

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,6 +9,6 @@ public class TestRunner {
         return Karate
                 .run("classpath:features")
                 .karateEnv("dev")
-                .tags("@End2EndAccount");
+                .tags("@Regression");
     }
 }
